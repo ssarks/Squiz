513b062e6f21fd538a266637775a3fd3fb903772
Sudipta Sarkar <ssarkar8110@gmail.com>
2019-06-11 04:55:51 -0400

    just added some dumy questions to test out my app.

---
+++ app.js
@@ -0,0 +1,9 @@
+var questions = [
+	new Questions("What is my first name?", ["Sudipta", "Sarkar", "Rudra", "Sarkar"], "Sudipta");
+	new Questions("What is my last name?", ["Sarkar", "Rudra", "Balto", "JFK"], "Sarkar");
+	new Questions("When is my date of birth?", ["December", "October", "November", "July"], "November");
+	new Questions("What sign am i?", ["Gemini", "Taurus", "Scorpio", "Aquarious"], "Scorpio");
+	new Questions("What is my most prized posession?", ["camera","guitar","computer","phone"], "camera");
+];
+
+var Quiz = new quiz(questions);
